4f55074eba42a5065d7c31ae4565cc1cc6fcee81
JaimeM28 <mirandaserranojaimemanuelnuel@gmail.com>
2023-01-02 20:59:45 -0600

    Algunas excepciones

---
+++ Biblioteca.java
@@ -0,0 +1,8 @@
+import Usuarios.Administrador;
+
+public class Biblioteca {
+    public static void main(String[] args) {
+        Administrador prueba = new Administrador("Jaime", "Miranda", 18);
+        prueba.registrarLibro();
+    }
+}
--- Usuarios/Administrador.java
+++ Usuarios/Administrador.java
@@ -2,28 +2,27 @@ package Usuarios;
 import java.util.*;
 import Biblioteca.*;
 import Builders.*;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 public class Administrador extends Usuario {
     private LibroBuilder builder = new LibroBuilder();
-
+    private Scanner sc = new Scanner(System.in);
 
     public Administrador(String nombre, String apellido,int edad){
         super(nombre, apellido, edad, new ArrayList<Libro>());
     }
 
     public void registrarLibro() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Ingresa el codigo: ");
-        builder.setCodigo(sc.nextInt());
-        System.out.print("\nIngresa el titulo: ");
-        builder.setTitulo(sc.nextLine());
-        System.out.print("\nIngresa el autor: ");
-        builder.setAutor(sc.nextLine());
-        System.out.print("\nIngresa el año de publicación: ");
-        builder.setCodigo(sc.nextInt());
-        System.out.print("\nIngresa el genero");
-        builder.setGenero(sc.nextLine());
-        System.out.print("\nIngresa la editorial");
-        builder.setEditorial(sc.nextLine());
+        builder.setCodigo(ingresarCodigo());
+        builder.setTitulo(IngresarTitulo());
+        builder.setAutor(ingresarString("autor"));
+        builder.setCodigo(ingresarAño());
+        builder.setGenero(ingresarString("genero"));
+        builder.setEditorial(ingresarString("editorial"));
         System.out.print("\nIngresa el número de páginas");
         builder.setNumPaginas(sc.nextInt());
         System.out.print("\nEstá disponible en físico?");
@@ -48,8 +47,120 @@ public class Administrador extends Usuario {
 
         Libro libro = this.builder.getResult();
 
-        
-
     }
+
+    private int ingresarCodigo(){
+        System.out.print("Ingresa el codigo de 6 dígitos: ");
+        try {
+            Integer codigo = Integer.parseInt(sc.nextLine());
+            return validarCodigo(codigo);
+        } catch (NumberFormatException e) {
+            System.out.println("Debes ingresar números");
+            ingresarCodigo();
+        }
+        return 0;
+        
+    }
+
+    private int validarCodigo(Integer codigo){
+        if(codigo.toString().length() != 6 ){
+            try {
+                throw new TamañoIncorrectoIntException("Se requiere un codigo de 6 digitos");
+            } catch (TamañoIncorrectoIntException ex) {
+                System.out.println(ex.getMessage());
+                ingresarCodigo();
+            }
+        }
+        return codigo;
+    }
+
+    private String IngresarTitulo(){
+        System.out.print("\nIngresa el titulo: ");
+        String titulo = sc.nextLine();
+        Pattern pattern = Pattern.compile("^[^\s][a-zA-Z]{1,}");  
+        Matcher matcher = pattern.matcher(titulo);
+        if(!matcher.find()){ 
+            try {
+                throw new VerificarCadenaException("Ingresa un nombre correcto");
+            } catch (VerificarCadenaException ex) {
+                System.out.println(ex.getMessage());
+                IngresarTitulo();
+            }
+        }
+        return titulo;
+    }
+
+    private String ingresarString(String dato){
+        System.out.printf("\nIngresa el %s: ", dato);
+        String autor = sc.nextLine();
+        Pattern pattern = Pattern.compile("^[A-Za-z\s]+$");  
+        Matcher matcher = pattern.matcher(autor);
+        if(!matcher.find()){ 
+            try {
+                throw new VerificarCadenaException("Ingresa un nombre correcto");
+            } catch (VerificarCadenaException ex) {
+                System.out.println(ex.getMessage());
+                ingresarString(dato);
+            }
+        }
+        return autor;
+    }
+
+    private int ingresarAño(){
+        System.out.print("Ingresa el codigo de 6 dígitos: ");
+        try {
+            Integer año = Integer.parseInt(sc.nextLine());
+            return validarTamañoAño(año);
+        } catch (NumberFormatException e) {
+            System.out.println("Debes ingresar números");
+            ingresarCodigo();
+        }
+        return 0;
+    }
+
+    private int validarTamañoAño(Integer año){
+        if(año.toString().length() <= 4  && año.toString().length() >1){
+            try {
+                throw new TamañoIncorrectoIntException("Se requiere un codigo de 6 digitos");
+            } catch (TamañoIncorrectoIntException ex) {
+                System.out.println(ex.getMessage());
+                ingresarAño();
+            }
+        }
+        return validarAño(año);
+    }
+
+    private int validarAño(Integer año){
+        if(año <= 2022 && año > 0 ){
+            try {
+                throw new VerificarRangoException("Se requiere un año entre 1 y 2022");
+            } catch ( VerificarRangoException ex) {
+                System.out.println(ex.getMessage());
+                ingresarAño();
+            }
+        }
+        return año;
+    }
+
+}
+
+class TamañoIncorrectoIntException extends Exception{
+    public TamañoIncorrectoIntException(String message) {
+        super(message);
+    }
+}
+
+class VerificarCadenaException extends Exception{
+    public VerificarCadenaException(String message) {
+        super(message);
+    }
+    
+} 
+
+class VerificarRangoException extends Exception{
+    public VerificarRangoException(String message){
+        super(message);
+    }
+
 
 }
